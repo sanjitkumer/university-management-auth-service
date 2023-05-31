074c6bb1c51272b7a090aa134c8370251d0641d6
sanjitkumer <sanjitk666@gmail.com>
2023-05-31 13:47:55 +0600

    added

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import config from './config/index';
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 async function bootstrap() {
-    try{
-        await mongoose.connect(config.database_url as string);
-        console.log(`Database is connecting successfully`);
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database is connecting successfully`)
 
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-          });
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('Failed to connect database', err)
+  }
+}
 
-    }catch(err){
-        console.log("Failed to connect database", err);
-    };
-};
-
-bootstrap ();
+bootstrap()
