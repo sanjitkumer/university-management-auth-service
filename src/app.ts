a87f59d98b006f1864daf03b025197c4c97a2a4a
sanjitkumer <sanjitk666@gmail.com>
2023-06-05 14:58:53 +0600

    add until controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
+// import usersService from './app/modules/users/users.service'
 const app: Application = express()
 // const port = 5000;
 
@@ -9,9 +11,18 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+//Application routes
+
+app.use('/api/v1/users/', usersRouter)
+
 //testing
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+app.get('/', async (req: Request, res: Response) => {
+  // await usersService.createUser({
+  //   id: '999',
+  //   password: '1234',
+  //   role: 'student',
+  // })
+  res.send('working Successfully')
 })
 
 export default app
